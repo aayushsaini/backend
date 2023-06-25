d3e23fdcee0d640ef074f0f304821564e178bbee
aayushsaini <aayush.r98@gmail.com>
2023-06-26 00:29:23 +0530

    User controller refactored

---
+++ src/controllers/user/user.controller.js
@@ -0,0 +1,44 @@
+import User from "../../models/user.model.js";
+import { messages } from "../../constants/messages.constant.js";
+import { validateUserData } from "./user.utils.js";
+
+/**
+ * Controller method to register a new user
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} response
+ */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      userName,
+      email,
+      password,
+      gender,
+      dateOfBirth,
+    } = req.body;
+
+    const validity = await validateUserData({ firstName, lastName, userName, email, password });
+    if (validity.response !== 200) return res.status(validity.response).json({ message: validity.message });
+
+    const newUser = await new User({
+      firstName,
+      lastName,
+      userName,
+      email,
+      password,
+      gender,
+      dateOfBirth,
+    }).save();
+    res
+      .status(200)
+      .json({ message: messages.success.controller.user.registered, newUser });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: messages.error.server.serverError, error });
+  }
+};
